7d68dce147ab4510ab57231e59c5b2a031359838
TeoIlie <teo.altum.quinque@gmail.com>
2025-04-11 15:03:39 -0400

    changed social icons

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
 
+interface SocialLink {
+  name: string;
+  url: string;
+  icon: string;
+}
+
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.scss'],
     standalone: false
 })
+
 export class AboutComponent {
-  // Add any properties or methods here
+  socialLinks: SocialLink[] = [
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/teodorilie/',
+      icon: 'fab fa-linkedin',
+    },
+    {
+      name: 'GitHub',
+      url: 'https://github.com/TeoIlie',
+      icon: 'fab fa-github',
+    },
+    {
+      name: 'YouTube',
+      url: 'https://www.youtube.com/@TeoTechnicTaken',
+      icon: 'fab fa-youtube',
+    },
+    {
+      name: 'Scholar',
+      url: 'https://scholar.google.ca/citations?user=YaQWjp8AAAAJ&hl=en',
+      icon: 'fas fa-graduation-cap',
+    },
+    {
+      name: 'Facebook',
+      url: 'https://www.facebook.com/profile.php?id=100004509104826',
+      icon: 'fab fa-facebook',
+    },
+    {
+      name: 'Strava',
+      url: 'https://www.strava.com/athletes/9039374',
+      icon: 'fab fa-strava',
+    },
+  ];
 }
--- src/app/socials/socials.component.ts
+++ src/app/socials/socials.component.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-interface SocialLink {
-  name: string;
-  url: string;
-  icon: string;
-}
-
 @Component({
     selector: 'app-socials',
     templateUrl: './socials.component.html',
@@ -61,37 +55,4 @@ export class SocialsComponent {
   }
 
   isSending = false;
-
-  socialLinks: SocialLink[] = [
-    {
-      name: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/teodorilie/',
-      icon: 'fab fa-linkedin',
-    },
-    {
-      name: 'GitHub',
-      url: 'https://github.com/TeoIlie',
-      icon: 'fab fa-github',
-    },
-    {
-      name: 'YouTube',
-      url: 'https://www.youtube.com/@TeoTechnicTaken',
-      icon: 'fab fa-youtube',
-    },
-    {
-      name: 'Scholar',
-      url: 'https://scholar.google.ca/citations?user=YaQWjp8AAAAJ&hl=en',
-      icon: 'fas fa-graduation-cap',
-    },
-    {
-      name: 'Facebook',
-      url: 'https://www.facebook.com/profile.php?id=100004509104826',
-      icon: 'fab fa-facebook',
-    },
-    {
-      name: 'Strava',
-      url: 'https://www.strava.com/athletes/9039374',
-      icon: 'fab fa-strava',
-    },
-  ];
 }
